0e9a28ff4dd89cb96eb3f42e141de3ab997da627
Omkar Dabade <od.omkard@gmail.com>
2021-08-24 18:00:01 +0530

    webhook added

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+from constants import BOT_TOKEN
 from Bot import updater
 import logging
+import os
 
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
-updater.start_polling()
+PORT = int(os.environ.get("PORT", "8443"))
+
+# updater.start_polling()
+updater.start_webhook(
+    listen="0.0.0.0",
+    port=PORT,
+    url_path=BOT_TOKEN,
+    webhook_url="https://earn-invest-grow.herokuapp.com/" + BOT_TOKEN,
+)
+
 
 # Run the bot until you press Ctrl-C or the process receives SIGINT,
 # SIGTERM or SIGABRT. This should be used most of the time, since
